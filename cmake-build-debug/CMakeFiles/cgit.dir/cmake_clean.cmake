76c0bb5cb031c67eaf43f5e9c27e19aeddcc32ca
Moritz Lemann <admin@example.com>
2020-12-11 12:16:11 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/cgit.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cgit.dir/main.c.o"
+  "cgit.exe"
+  "cgit.pdb"
+  "libcgit.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cgit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
